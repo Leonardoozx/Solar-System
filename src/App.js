de6234b8810a04b699fc0701d4fca54a18eb887c
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-05-09 15:16:51 -0300

    add the Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      // <p>Sistema Solar</p>
+      // <header><h1>Sistema Solar</h1></header>
+      <Header />
+    );
   }
 }
 
