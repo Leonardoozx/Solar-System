4497ff64d939cdba60e1db81b53f5262f6868ecf
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-05-09 17:59:43 -0300

    complete the 6th requirement

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Title from './Title';
+import Planets from '../data/planets';
+
+const AllTheInfos = Planets.map((x) => {
+  const { name: planetName, image: planetImage } = x;
+  return (
+    <div data-testid="planet-card" key={ planetName }>
+      <p data-testid="planet-name">{planetName}</p>
+      <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+    </div>
+  );
+});
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {AllTheInfos}
       </div>
     );
   }
