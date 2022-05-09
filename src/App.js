ee4eecb186a512cdceca1b44897edb2d2f8fc991
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-05-09 18:05:37 -0300

    fix an issue at PlanetCard,jsx component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-// import Title from './components/Title';
 
 class App extends React.Component {
   render() {
     return (
-      <body>
+      <main>
         <Header />
         <SolarSystem />
-      </body>
+      </main>
     );
   }
 }
